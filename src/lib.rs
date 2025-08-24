945dd05bd72327c0d0731a321e66b84b144c2f5b
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-08-24 10:47:53 +0200

    Refactor email scheduling function and update manifest to include email and body parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,22 @@
 mod exchange_outpost;
-use crate::exchange_outpost::FinData;
+use crate::exchange_outpost::{FinData, schedule_email};
 use extism_pdk::{FnResult, Json, ToBytes, encoding, plugin_fn};
 use serde::Serialize;
 
 #[derive(Serialize, ToBytes)]
 #[encoding(Json)]
-pub struct Output {}
+pub struct Output {
+    email: String,
+    body: String,
+}
 
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<Output> {
-    Ok(Output {})
+    let email_target = fin_data.get_call_argument::<String>("email")?;
+    let body = fin_data.get_call_argument::<String>("body")?;
+    schedule_email(&email_target, &body)?;
+    Ok(Output {
+        email: email_target,
+        body,
+    })
 }
